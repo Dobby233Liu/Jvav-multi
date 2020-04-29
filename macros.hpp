9da7c96edd5a11b5e301446fe7c18686ae3c1646
Liu <15816141883@163.com>
2020-04-29 13:45:11 +0800

    stuff

--- macros.hpp
+++ macros.hpp
@@ -3,6 +3,6 @@
 #define REPL_VERSION (string)"20w06a"
 #define REPL_VERSION_ID 7
 #define RUNTIME_VERSION (string)"1.2.10889"
-#define VERSION_API "https://30266-Official.github.io/updates" // no trailing slash
+#define VERSION_API "https://raw.githubusercontent.com/Dobby233Liu/Jvav-multi/master" // no trailing slash
 #define AUTHOR (string)"30266, TZG"
-#define DL_ADDR (string)"https://30266-official.github.io/updates/Jvav.zip"
+#define DL_ADDR (string)"https://github.com/Dobby233Liu/Jvav-multi"
