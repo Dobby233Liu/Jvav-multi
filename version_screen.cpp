b40a0e691c8ef4a78c647e7c24cc5562457aec58
Liu <15816141883@163.com>
2020-04-29 11:44:45 +0800

    Anti-sue part 3, notice TZG's authorship

--- version_screen.cpp
+++ version_screen.cpp
@@ -2,7 +2,7 @@ void version_screen(string language, int intro = TRUE){
 	cout << "--------------------------------------------\n";
     cout << "| Jvav REPL              Runtime "+RUNTIME_VERSION+" |\n";
     cout << "| By Dr. HaoYang Zhang         REPL "+REPL_VERSION+" |\n";
-    cout << "| programmed by "+AUTHOR+"                      |\n";
+    cout << "| programmed by "+AUTHOR+"                 |\n";
 if (intro){ // srry for the indent. it was intended
 	cout << "| Type '.help' to get info                 |\n";
 	cout << "| WHAT'S NEW:                              |\n";
