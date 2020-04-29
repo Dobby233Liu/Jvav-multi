896d4e8895b1d9484c2f950b0001420358e57929
Liu <15816141883@163.com>
2020-04-29 11:47:44 +0800

    Anti-sue part 4: last line in .ver deletion

--- commands/repl/version.cpp
+++ commands/repl/version.cpp
@@ -1,3 +1,3 @@
 version_screen(language, FALSE);
 cout << "\nIt's just a joke, but we still made it. This joke was originally brought by Zhang Haoyang.";
-cout << "\nThis version supports running without JDK.\nProgram written by 30266.\n";
+cout << "\nThis version supports running without JDK.\n";
