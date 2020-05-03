9b09df3f8400ecc587dfb2ec71aafc859056ee45
Liu <15816141883@163.com>
2020-05-03 16:39:40 +0800

    publish

--- macros.hpp
+++ macros.hpp
@@ -1,7 +1,7 @@
 #define TRUE 1 // i'm a fool
 #define FALSE 0
 #define REPL_VERSION (string)"20w06f"
-#define REPL_VERSION_ID 11
+#define REPL_VERSION_ID 12
 #define RUNTIME_VERSION (string)"1.2.10891"
 #define VERSION_API "https://raw.githubusercontent.com/Dobby233Liu/Jvav-multi/master" // no trailing slash
 #define AUTHOR (string)"30266&&TZG"
